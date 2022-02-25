bc6dad90b72f9f8a3038463abb0531ad639f8d50
ThiagoTSilva <https://ThiagoTSilva@github.com>
2022-02-25 01:13:34 -0300

    refatorando

---
+++ src/FrwkQuickWait.Application/Services/ConsumerService.cs
@@ -0,0 +1,59 @@
+﻿using Confluent.Kafka;
+using FrwkQuickWait.Domain.Constants;
+using FrwkQuickWait.Domain.Intefaces;
+using Microsoft.Extensions.Configuration;
+
+namespace FrwkQuickWait.Service.Services
+{
+    public class ConsumerService : IConsumerService
+    {
+        private readonly ConsumerConfig config;
+        private readonly IConfiguration _configuration;
+
+        public ConsumerService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            this.config = new ConsumerConfig
+            {
+                BootstrapServers = _configuration.GetSection("Kafka")["host"],
+                EnableAutoOffsetStore = false,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+        }
+
+        public async Task<ConsumeResult<Ignore, string>> ProcessQueue(string topicName)
+        {
+            bool? cancelled = false;
+            var cancellationToken = new CancellationToken();
+            var consumeResult = new ConsumeResult<Ignore, string>();
+            config.GroupId = $"{topicName}-group-2";
+            using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            consumer.Subscribe(topicName);
+
+            try
+            {
+
+                while ((bool)!cancelled)
+                {
+                    try
+                    {
+
+                        consumeResult = consumer.Consume(cancellationToken);
+                        cancelled = true;
+                    }
+                    catch (ConsumeException ex) { consumer.Close(); }
+
+                }
+
+                consumer.Close();
+            }
+            catch (OperationCanceledException ex)
+            {
+                consumer.Close();
+            }
+
+            return await Task.FromResult(consumeResult);
+        }
+    }
+}
---
+++ src/FrwkQuickWait.Application/Services/ProducerService.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using FrwkQuickWait.Domain.Constants;
+using FrwkQuickWait.Domain.Entity;
+using FrwkQuickWait.Domain.Intefaces;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace FrwkQuickWait.Service.Services
+{
+    public class ProducerService : IProducerService
+    {
+        private readonly ClientConfig cloudConfig;
+        private readonly IConfiguration _configuration;
+        
+        public ProducerService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            cloudConfig = new ClientConfig
+            {
+                BootstrapServers = _configuration.GetSection("Kafka")["host"]
+            };
+        }
+
+        public async Task Call(MessageInput message, string topicName)
+        {
+            var stringfiedMessage = JsonConvert.SerializeObject(message);
+
+            using var producer = new ProducerBuilder<string, string>(cloudConfig).Build();
+
+            var key = new Guid().ToString();
+
+            await producer.ProduceAsync(topicName, new Message<string, string> { Key = key, Value = stringfiedMessage });
+
+            producer.Flush(TimeSpan.FromSeconds(10));
+
+        }
+    }
+}
